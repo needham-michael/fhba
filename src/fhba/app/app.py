66c897eafe1bbd6ad22b596543110a73b4191c91
needham-michael <153653339+needham-michael@users.noreply.github.com>
2026-04-10 16:13:11 -0500

    reformulated UI into tabs for download/classify/aggregate

--- src/fhba/app/app.py
+++ src/fhba/app/app.py
@@ -69,18 +69,21 @@ class StageSetup(param.Parameterized):
         return pn.Row(self.view,)
 
 def build_app():
-    
-    pipeline_daily = pn.pipeline.Pipeline(debug=True)
-    pipeline_daily.add_stage(name="Select Instrument",stage=StageSetup)
-    pipeline_daily.add_stage(name="Download Previews",stage=StageDownloadPreviews)
-    pipeline_daily.add_stage(name="Sort Images",stage=StagePreviewImages)
-    pipeline_daily.add_stage(name="Download Granules",stage=StageDownloadGranules)
-    pipeline_daily.add_stage(name="Analyze Pixels",stage=StageAnalyze)
-    pipeline_daily.add_stage(name="Categorize",stage=StageClassify)
 
-    pipeline_ytd = pn.pipeline.Pipeline(debug=True)
-    pipeline_ytd.add_stage(name="Select Instrument",stage=StageSetup)
-    pipeline_ytd.add_stage(name="Aggregate Burn Masks",stage=StageAggregate)
+    pipeline_download = pn.pipeline.Pipeline(debug=True)
+    pipeline_download.add_stage(name="Select Instrument",stage=StageSetup)
+    pipeline_download.add_stage(name="Download Previews",stage=StageDownloadPreviews)
+    pipeline_download.add_stage(name="Sort Images",stage=StagePreviewImages)
+    pipeline_download.add_stage(name="Download Granules",stage=StageDownloadGranules)
+    
+    pipeline_classify = pn.pipeline.Pipeline(debug=True)
+    pipeline_classify.add_stage(name="Select Instrument",stage=StageSetup)
+    pipeline_classify.add_stage(name="Analyze Pixels",stage=StageAnalyze)
+    pipeline_classify.add_stage(name="Categorize",stage=StageClassify)
+
+    pipeline_aggregate = pn.pipeline.Pipeline(debug=True)
+    pipeline_aggregate.add_stage(name="Select Instrument",stage=StageSetup)
+    pipeline_aggregate.add_stage(name="Aggregate Burn Masks",stage=StageAggregate)
 
     # Placeholder for tab setup to separate YTD burn mask aggregation and stats
     with open("README.md") as f:
@@ -90,9 +93,11 @@ def build_app():
         pn.pane.Markdown("# Flint Hills Burned Area (FHBA) Mapping Tool"),
         pn.layout.Divider(),
         pn.Tabs(
-            ("Daily Images",pipeline_daily),
-            ("YTD Burn Mask",pipeline_ytd),
             ("README",pn.pane.Markdown(readme_md, width=800)),
+            ("1. Download Granules",pipeline_download),
+            ("2. Classify Pixels",pipeline_classify),
+            ("3. Aggregate YTD Burn Masks",pipeline_aggregate),
+            tabs_location='above',
     )
     )
         
